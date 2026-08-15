150632d5f6d2e5bf5468644ec49ee690bfe2f308
Adam <tb.adam.co.id@gmail.com>
2026-08-15 14:17:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,23 +24,6 @@ async def on_message(message):
         await message.channel.send("\U0001f642")
     elif message.content.startswith("$genpass"):
         await message.channel.send(gen_pass(10))
-    elif message.content.startswith('$guess'):
-            await message.channel.send('Guess a number between 1 and 10.')
-
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
-
-            answer = random.randint(1, 10)
-
-            try:
-                guess = await client.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send(f'Oops. It is actually {answer}.')
     else:
             await message.channel.send(message.content)
 
